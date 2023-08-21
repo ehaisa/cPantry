cb41a918cefdcf7ab193f881f783be3e3952e7f5
ehaisa <isalaura1304@gmail.com>
2023-08-21 12:00:59 -0300

    Criando tela com informações

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <locale.h>
 
 void menuPrincipal(void);
+void telaInfos(void);
 
 /// Programa Principal ///
 
 int main(void) {
     setlocale(LC_ALL,"Portuguese_Brazil");
     menuPrincipal();
+    telaInfos();
 }
 
 /// Funções ///
@@ -33,6 +35,31 @@ void menuPrincipal(void) {
     printf("///            4. Relatórios                                                ///\n");
     printf("///            5. Sobre o Sistema                                           ///\n");
     printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void telaInfos(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///               Universidade Federal do Rio Grande do Norte               ///\n");
+    printf("///                   Centro de Ensino Superior do Seridó                   ///\n");
+    printf("///                 Departamento de Computação e Tecnologia                 ///\n");
+    printf("///                    Disciplina DCT1106 -- Programação                    ///\n");
+    printf("///            Projeto Sistema de Controle de Despensa Doméstica            ///\n");
+    printf("///                 Developed by @ehaisa -- since Aug, 2023                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                  = = = = = My Home Pantry = = = = =                     ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Esse programa não foi feito para uso comercial, tendo uma natureza     ///\n");
+    printf("///  estritamente acadêmica, sendo feito para a aplicação prática dos       ///\n");
+    printf("///  conceitos repassados e discutidos em sala de aula. O programa também   ///\n");
+    printf("///  tem propósito avaliativo, sendo uma ferramenta para avaliar o          ///\n");
+    printf("///  desempenho da desenvolvedora e sua compreensão do assunto.             ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
