6e88f600187be71b6102478ca1ae9446e34fbe08
ehaisa <isalaura1304@gmail.com>
2023-08-21 17:18:55 -0300

    Criando tela das categorias dos itens

--- main.c
+++ main.c
@@ -3,21 +3,26 @@
 
 #include <stdio.h>
 #include <locale.h> // Biblioteca necessaria para corrigir os acentos
+#include <stdlib.h>
 
 void menuPrincipal(void);
 void telaInfos(void);
 void telaDev(void);
+void telaItens(void);
+void itensLimpeza(void);
 
 /// Programa Principal ///
 
 int main(void) {
     setlocale(LC_ALL,"Portuguese_Brazil"); // Corrige os acentos, fonte: https://youtu.be/hB6KFP6luL4
     menuPrincipal();
+    telaItens();
+    itensLimpeza();
     telaInfos();
     telaDev();
 }
 
-/// Funções ///
+/// Telas ///
 
 void menuPrincipal(void) {
     char op;
@@ -41,6 +46,71 @@ void menuPrincipal(void) {
     printf("///            3. Editar Despensa                                           ///\n");
     printf("///            4. Relatórios                                                ///\n");
     printf("///            5. Sobre o Sistema                                           ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///            Qual opção deseja acessar? (Informe o número) ");
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void telaItens(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///               ===========================================               ///\n");
+    printf("///               =                                         =               ///\n");
+    printf("///               =              My Home Pantry             =               ///\n");
+    printf("///               =                                         =               ///\n");
+    printf("///               ===========================================               ///\n");
+    printf("///                 Developed by @ehaisa -- since Aug, 2023                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                 = = = = = Itens Armazenados = = = = =                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Limpeza Doméstica                                         ///\n");
+    printf("///            2. Higiene Pessoal                                           ///\n");
+    printf("///            3. Alimentação                                               ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///            Qual opção deseja acessar? (Informe o número) ");
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void itensLimpeza(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///               ===========================================               ///\n");
+    printf("///               =                                         =               ///\n");
+    printf("///               =              My Home Pantry             =               ///\n");
+    printf("///               =                                         =               ///\n");
+    printf("///               ===========================================               ///\n");
+    printf("///                 Developed by @ehaisa -- since Aug, 2023                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                 = = = = = Limpeza Doméstica = = = = =                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Pesquisar Item                                            ///\n");
+    printf("///            2. Visualizar Todos                                          ///\n");
     printf("///            0. Sair                                                      ///\n");
     printf("///                                                                         ///\n");
     printf("///////////////////////////////////////////////////////////////////////////////\n");
